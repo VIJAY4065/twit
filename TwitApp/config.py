892ba985f2189b7d930346cafb3e002ce2219e6b
VIJAY4065 <41334489+VIJAY4065@users.noreply.github.com>
2020-02-03 20:51:18 +0530

    Update config.py

--- TwitApp/config.py
+++ TwitApp/config.py
@@ -3,8 +3,7 @@ import os
 
 class Config(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
-                                             'postgresql://localhost/dfn59a7e5u2a21')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','postgres://aohwcepllagdbo:0e92b0ac95c35b58facbd6bd7ff5d7b4f76584a7de6a73ddcddc0f0a6f846ad9@ec2-54-247-188-107.eu-west-1.compute.amazonaws.com:5432/dfn59a7e5u2a21')
     PROPAGATE_EXCEPTIONS = True
     SECRET_KEY = os.environ \
                    .get('SECRET_KEY',
